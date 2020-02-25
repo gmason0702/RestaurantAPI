cafbc5c467530eb52df8ef793b7950f798d42f64
G Mason <gmason0702@gmail.com>
2020-02-25 10:48:30 -0500

    idk

---
+++ RestaurantRater/Models/RestaurantDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext() :base("DefaultConnection")//connects to the name of the DbContext, which connects to the local database
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }//DbSet requires taking in a TEntity class
+    }
+}
